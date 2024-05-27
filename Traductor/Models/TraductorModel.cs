e89a3d538ffd942d8acfbb2eba4cdbc037782d3e
Johan Merchan <jsmerchan1013@hotmail.com>
2024-05-26 21:03:47 -0500

    correccion direcciones svg

--- Traductor/Models/TraductorModel.cs
+++ Traductor/Models/TraductorModel.cs
@@ -6,42 +6,42 @@ namespace Traductor.Models
         private readonly Dictionary<char, string> svgDictionary = new Dictionary<char, string>
         {
 
-                    { '0', "../Caracteres/0.svg" },
-                    { '1', "../Caracteres/1.svg" },
-                    { '2', "../Caracteres/2.svg" },
-                    { '3', "../Caracteres/3.svg" },
-                    { '4', "../Caracteres/4.svg" },
-                    { '5', "../Caracteres/5.svg" },
-                    { '6', "../Caracteres/6.svg" },
-                    { '7', "../Caracteres/7.svg" },
-                    { '8', "../Caracteres/8.svg" },
-                    { '9', "../Caracteres/9.svg" },
-                    { 'a', "../Caracteres/a.svg" },
-                    { 'b', "../Caracteres/b.svg" },
-                    { 'c', "../Caracteres/c.svg" },
-                    { 'd', "../Caracteres/d.svg" },
-                    { 'e', "../Caracteres/e.svg" },
-                    { 'f', "../Caracteres/f.svg" },
-                    { 'g', "../Caracteres/g.svg" },
-                    { 'h', "../Caracteres/h.svg" },
-                    { 'i', "../Caracteres/i.svg" },
-                    { 'j', "../Caracteres/j.svg" },
-                    { 'k', "../Caracteres/k.svg" },
-                    { 'l', "../Caracteres/l.svg" },
-                    { 'm', "../Caracteres/m.svg" },
-                    { 'n', "../Caracteres/n.svg" },
-                    { 'o', "../Caracteres/o.svg" },
-                    { 'p', "../Caracteres/p.svg" },
-                    { 'q', "../Caracteres/q.svg" },
-                    { 'r', "../Caracteres/r.svg" },
-                    { 's', "../Caracteres/s.svg" },
-                    { 't', "../Caracteres/t.svg" },
-                    { 'u', "../Caracteres/u.svg" },
-                    { 'v', "../Caracteres/v.svg" },
-                    { 'w', "../Caracteres/w.svg" },
-                    { 'x', "../Caracteres/x.svg" },
-                    { 'y', "../Caracteres/y.svg" },
-                    { 'z', "../Caracteres/z.svg" }
+                    { '0', "/Caracteres/0.svg" },
+                    { '1', "/Caracteres/1.svg" },
+                    { '2', "/Caracteres/2.svg" },
+                    { '3', "/Caracteres/3.svg" },
+                    { '4', "/Caracteres/4.svg" },
+                    { '5', "/Caracteres/5.svg" },
+                    { '6', "/Caracteres/6.svg" },
+                    { '7', "/Caracteres/7.svg" },
+                    { '8', "/Caracteres/8.svg" },
+                    { '9', "/Caracteres/9.svg" },
+                    { 'a', "/Caracteres/A.svg" },
+                    { 'b', "/Caracteres/B.svg" },
+                    { 'c', "/Caracteres/C.svg" },
+                    { 'd', "/Caracteres/D.svg" },
+                    { 'e', "/Caracteres/E.svg" },
+                    { 'f', "/Caracteres/F.svg" },
+                    { 'g', "/Caracteres/G.svg" },
+                    { 'h', "/Caracteres/H.svg" },
+                    { 'i', "/Caracteres/I.svg" },
+                    { 'j', "/Caracteres/J.svg" },
+                    { 'k', "/Caracteres/K.svg" },
+                    { 'l', "/Caracteres/L.svg" },
+                    { 'm', "/Caracteres/M.svg" },
+                    { 'n', "/Caracteres/N.svg" },
+                    { 'o', "/Caracteres/O.svg" },
+                    { 'p', "/Caracteres/P.svg" },
+                    { 'q', "/Caracteres/Q.svg" },
+                    { 'r', "/Caracteres/R.svg" },
+                    { 's', "/Caracteres/S.svg" },
+                    { 't', "/Caracteres/T.svg" },
+                    { 'u', "/Caracteres/U.svg" },
+                    { 'v', "/Caracteres/V.svg" },
+                    { 'w', "/Caracteres/W.svg" },
+                    { 'x', "/Caracteres/X.svg" },
+                    { 'y', "/Caracteres/Y.svg" },
+                    { 'z', "/Caracteres/Z.svg" }
         };
             
             public string ObtenerRutaSvg(char letra)
@@ -52,7 +52,7 @@ namespace Traductor.Models
                 }
                 else
                 {
-                    return "../Caracteres/3.svg";
+                    return "/Caracteres/3.svg";
                 }
             }
         }
